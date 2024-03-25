0e792928e760f544eb269794b9faae3e52f8baff
Leonardo Araujo <leo.san9@hotmail.com>
2024-03-25 09:44:25 -0300

    fix font import

--- src/globals-styles.ts
+++ src/globals-styles.ts
@@ -1,17 +1,15 @@
 import { createGlobalStyle,  } from 'styled-components';
-import GothamPro from './fonts/gotham/gotham.ttf';
-import Inter from './fonts/inter/inter.ttf';
 
 export const GlobalStyle = createGlobalStyle`
     @font-face {
         font-family: 'GothamPro';
-        src: url(${GothamPro}) format('woff');
+        src: url('/fonts/gotham.ttf') format('woff');
         font-weight: 300;
         font-style: normal;
     }
     @font-face {
         font-family: 'Inter';
-        src: url(${Inter}) format('woff');
+        src: url('/fonts/inter.ttf') format('woff');
         font-weight: 300;
         font-style: normal;
     }
