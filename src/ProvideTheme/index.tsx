a2288fa9f5b7930fcb6127cc13ca43e36843a893
Leonardo Araujo <leo.san9@hotmail.com>
2024-03-25 14:46:36 -0300

    fix font to base64

--- src/ProvideTheme/index.tsx
+++ src/ProvideTheme/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-// import { useTheme } from '../ThemeProvider';
+import { useTheme } from '../ThemeProvider';
 import light from '../styles/json/light.json';
-// import dark from '../styles/json/dark.json';
+import dark from '../styles/json/dark.json';
 import { GlobalStyle } from '../globals-styles';
 
 export const ProvideTheme = ({ children }: any) => {
-  // const { theme } = useTheme();
+  const { theme } = useTheme();
   return (
-    <ThemeProvider theme={light}>
+    <ThemeProvider theme={theme === 'dark' ? dark : (light as typeof dark)}>
       <GlobalStyle />
       {children}
     </ThemeProvider>
